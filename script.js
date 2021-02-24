8d8cacf3155189e741c8e63b2c0c2c3c319a957a
Aryaman <shrivastavaaryaman0702@gmail.com>
2021-02-24 21:17:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,116 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'The great Victoria Desert is located in',
+    answers: [
+      { text: 'Canada', correct: false },
+      { text: 'West Africa', correct: false },
+      { text: 'Australia', correct: true },
+      { text: 'North America', correct: false }
+    ]
+  },
+  {
+    question: 'The landmass of which of the following continent is the least?',
+    answers: [
+      { text: 'Africa', correct: false },
+      { text: 'Asia', correct: false },
+      { text: 'Australia', correct: true },
+      { text: 'Europe', correct: false }
+    ]
+  },
+  {
+    question: 'Which one of the following is Tropical grassland?',
+    answers: [
+      { text: 'Tiaga', correct: false },
+      { text: 'Savannah', correct: true },
+      { text: 'Pampas', correct: false },
+      { text: 'Pairies', correct: false }
+    ]
+  },
+  {
+    question: 'The temperature increases rapidly after',
+    answers: [
+      { text: 'Inosphere', correct: true },
+      { text: 'Exosphere', correct: false },
+      { text: 'Stratosphere', correct: false },
+      { text: 'Troposphere', correct: false }
+    ]
+  }
+]
