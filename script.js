a500a5378a2151458b7b3f0ca127a6e6938fc32c
Aryaman <shrivastavaaryaman0702@gmail.com>
2021-03-01 21:33:53 +0530

    new

--- script.js
+++ script.js
@@ -91,8 +91,8 @@ const questions = [
     answers: [
       { text: 'https://yahoo.com', correct: false },
       { text: 'https://hotmail.com/SignUp', correct: false },
-      { text: 'https://accounts.google.com/SignUpWithoutGmail?hl=e', correct: true },
-      { text: 'None of the above', correct: false }
+      { text: 'None of the above', correct: false },
+      { text: 'https://accounts.google.com/SignUpWithoutGmail?hl=e', correct: true }
     ]
   },
   {
@@ -108,9 +108,45 @@ const questions = [
     question: 'Which is not a colour of google?',
     answers: [
       { text: 'Green', correct: false },
+      { text: 'Orange', correct: true },
       { text: 'Red', correct: false },
-      { text: 'Peach', correct: false },
-      { text: 'Orange', correct: true }
+      { text: 'Peach', correct: false }
+    ]
+  },
+  {
+    question: 'Where are documents that you place on Google Drive stored?',
+    answers: [
+      { text: 'On your school network', correct: false },
+      { text: 'On a flash drive', correct: false },
+      { text: 'In the cloud', correct: true },
+      { text: 'Somewhere in claifornia', correct: false }
+    ]
+  },
+  {
+    question: 'What is the title of Google new browser?',
+    answers: [
+      { text: 'Google Chrome', correct: true },
+      { text: 'Moxilla', correct: false },
+      { text: 'Chrominism', correct: false },
+      { text: 'Google Chromous', correct: false }
+    ]
+  },
+  {
+    question: 'The word "Google" originated from a misspelling of:',
+    answers: [
+      { text: 'Googad', correct: false },
+      { text: 'Googol', correct: true },
+      { text: 'Goolink', correct: false },
+      { text: 'Goosearch', correct: false }
+    ]
+  },
+  {
+    question: 'Google was founded by Larry Page and who else?',
+    answers: [
+      { text: 'Terry Winograd', correct: false },
+      { text: 'Steve Jobs', correct: false },
+      { text: 'Sergey Brin', correct: true },
+      { text: 'Bill Gates', correct: false }
     ]
   }
 ]
